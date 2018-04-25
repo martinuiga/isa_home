1401234d57e630dcea4ee1c1634bf16b8036e1ae
Uiga <karl-martin.uiga@cgi.com>
2018-04-25 16:35:53 +0300

    Message works almost!

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
+/*if(empty($_POST['name'])  		||
     empty($_POST['email']) 		||
     empty($_POST['phone']) 		||
     empty($_POST['message'])	||
@@ -8,7 +8,7 @@ if(empty($_POST['name'])  		||
 {
     echo "No arguments Provided!";
     return false;
-}
+}*/
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
@@ -16,20 +16,35 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 # Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 use Mailgun\Mailgun;
 
-# First, instantiate the SDK with your API credentials
-$mg = Mailgun::create('key-key-57d810c15a260711ee2e29e382182395');
-
+/*# First, instantiate the SDK with your API credentials
+$mg = new Mailgun('key-57d810c15a260711ee2e29e382182395');
+$domain = "https://app.mailgun.com/app/domains/sandbox50e17ddd4a0145e3a8c4cf678648b772.mailgun.org";
 # Now, compose and send your message.
 # $mg->messages()->send($domain, $params);
-$mg->messages()->send('https://app.mailgun.com/app/domains/sandbox50e17ddd4a0145e3a8c4cf678648b772.mailgun.org', [
-    'from'    => 'bob@example.com',
+
+$mg->messages()->send($domain, array(
+    'from'    => 'uigakarlmartin@gmail.com',
     'to'      => 'uigakarlmartin@gmail.com',
     'subject' => 'The PHP SDK is awesome!',
     'text'    => 'It is so simple to send a message.'
-]);
+));*/
+
+
+
+$mg = new Mailgun('key-57d810c15a260711ee2e29e382182395');
+$domain = "https://app.mailgun.com/app/domains/sandbox50e17ddd4a0145e3a8c4cf678648b772.mailgun.org";
+
+//Customise the email - self explanatory
+$mg->sendMessage($domain, array(
+        'from: uigakarlmartin@gmail.com',
+        'to: uigakarlmartin@gmail.com',
+        'subject: The PHP SDK is awesome!',
+        'text: It is so simple to send a message.'
+    )
+);
 
 # You can see a record of this email in your logs: https://app.mailgun.com/app/logs .
 
